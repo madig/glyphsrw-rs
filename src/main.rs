1d9aec1d4f2965aa9d1a688f9bb051daa6bb8cef
Nikolaus Waxweiler <madigens@gmail.com>
2019-11-14 00:03:26 +0000

    Some more structuring, explode on unknown fields

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,15 @@ use std::io::prelude::Read;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
 struct Font {
     #[serde(rename = ".appVersion")]
     app_version: String,
+    classes: Option<Vec<Feature>>,
     copyright: Option<String>,
     custom_parameters: Option<Vec<CustomParameter>>,
+    #[serde(rename = "DisplayStrings")]
+    display_strings: Option<Vec<String>>,
     date: String,
     designer: Option<String>,
     family_name: String,
@@ -18,6 +22,7 @@ struct Font {
     font_master: Vec<FontMaster>,
     glyphs: Vec<Master>,
     instances: Option<Vec<Instance>>,
+    kerning: Option<HashMap<String, HashMap<String, HashMap<String, String>>>>,
     units_per_em: String,
     user_data: Option<HashMap<String, serde_json::Value>>,
     version_major: String,
@@ -25,12 +30,22 @@ struct Font {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 struct CustomParameter {
     name: String,
     value: serde_json::Value,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+struct Guideline {
+    angle: Option<String>,
+    locked: Option<String>,
+    position: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 struct Feature {
     automatic: Option<String>,
     code: String,
@@ -39,12 +54,14 @@ struct Feature {
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
 struct FontMaster {
     alignment_zones: Option<Vec<String>>,
     ascender: String,
     cap_height: String,
     custom_parameters: Option<Vec<CustomParameter>>,
     descender: String,
+    guide_lines: Option<Vec<Guideline>>,
     horizontal_stems: Option<Vec<String>>,
     id: String,
     user_data: Option<HashMap<String, serde_json::Value>>,
@@ -56,40 +73,133 @@ struct FontMaster {
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
 struct Instance {
     custom_parameters: Option<Vec<CustomParameter>>,
     exports: Option<String>,
     instance_interpolations: HashMap<String, String>,
     interpolation_weight: Option<String>,
+    manual_interpolation: Option<String>,
     name: String,
     weight_class: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
 struct Master {
+    category: Option<String>,
+    color: Option<String>,
+    export: Option<String>,
     glyphname: String,
+    last_change: Option<String>,
+    parts_settings: Option<Vec<PartsSetting>>,
     layers: Vec<Layer>,
+    left_kerning_group: Option<String>,
+    left_metrics_key: Option<String>,
     note: Option<String>,
+    right_kerning_group: Option<String>,
+    right_metrics_key: Option<String>,
+    script: Option<String>,
+    sub_category: Option<String>,
     unicode: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
 struct Layer {
     anchors: Option<Vec<Anchor>>,
+    annotations: Option<Vec<Annotation>>,
+    associated_master_id: Option<String>,
+    background_image: Option<BackgroundImage>,
+    background: Option<BackgroundLayer>,
+    components: Option<Vec<Component>>,
+    guide_lines: Option<Vec<Guideline>>,
+    hints: Option<Vec<Hint>>,
     layer_id: String,
+    name: Option<String>,
     paths: Option<Vec<Path>>,
+    user_data: Option<HashMap<String, serde_json::Value>>,
     width: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+struct BackgroundLayer {
+    anchors: Option<Vec<Anchor>>,
+    components: Option<Vec<Component>>,
+    layer_id: Option<String>,
+    paths: Option<Vec<Path>>,
+    width: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 struct Anchor {
     name: String,
     position: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+struct Component {
+    name: String,
+    transform: Option<String>,
+    piece: Option<ComponentPiece>,
+    anchor: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+struct ComponentPiece {
+    height: Option<String>,
+    crotch_depth: Option<String>,
+    shoulder_width: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+struct PartsSetting {
+    bottom_name: String,
+    bottom_value: String,
+    name: String,
+    top_name: String,
+    top_value: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+struct Annotation {
+    angle: Option<String>,
+    position: String,
+    text: Option<String>,
+    width: Option<String>,
+    r#type: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+struct BackgroundImage {
+    crop: String,
+    image_path: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+struct Hint {
+    horizontal: String,
+    origin: String,
+    target: String,
+    r#type: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 struct Path {
     closed: String,
     nodes: Vec<String>,
@@ -105,17 +215,14 @@ fn main() -> std::io::Result<()> {
     // let mut data_file = File::open("data/GlyphsUnitTestSans.json")?;
     // let mut data_file = File::open("data/BracketTestFont2.json")?;
     // let mut data_file = File::open("data/BraceTestFont.json")?;
-    // let mut data_file = File::open("data/AnchorAttachmentTest.json")?;
-    let mut data_file = File::open("data/NewFont.json")?;
+    let mut data_file = File::open("data/AnchorAttachmentTest.json")?;
+    // let mut data_file = File::open("data/NewFont.json")?;
     // let mut data_file = File::open("data/Empty.json")?;
     let mut contents = String::new();
     data_file.read_to_string(&mut contents)?;
 
     let font: Font = serde_json::from_str(&contents).unwrap();
-    println!(
-        "{}",
-        ron::ser::to_string_pretty(&font, ron::ser::PrettyConfig::default()).unwrap()
-    );
+    println!("{}", ron::ser::to_string_pretty(&font, ron::ser::PrettyConfig::default()).unwrap());
 
     Ok(())
 }
