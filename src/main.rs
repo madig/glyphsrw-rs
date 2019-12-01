42290ac0f2ab94e5029d49e8654285811b5be37a
Nikolaus Waxweiler <madigens@gmail.com>
2019-12-01 21:40:57 +0000

    more WIPping

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use plist;
+use plist; // XXX: Disabled integer guess for now for testing.
+use serde::de::{Deserializer, Visitor};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::prelude::Read;
+// use std::fs::File;
+// use std::io::prelude::Read;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -17,6 +18,8 @@ struct Font {
     display_strings: Option<Vec<String>>,
     date: String,
     designer: Option<String>,
+    #[serde(rename = "designerURL")]
+    designer_url: Option<String>,
     family_name: String,
     feature_prefixes: Option<Vec<Feature>>,
     features: Option<Vec<Feature>>,
@@ -24,10 +27,10 @@ struct Font {
     glyphs: Vec<Master>,
     instances: Option<Vec<Instance>>,
     kerning: Option<HashMap<String, HashMap<String, HashMap<String, String>>>>,
-    units_per_em: u32,
+    units_per_em: String,
     user_data: Option<HashMap<String, serde_json::Value>>,
-    version_major: u32,
-    version_minor: u32,
+    version_major: String,
+    version_minor: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -41,6 +44,7 @@ struct CustomParameter {
 #[serde(deny_unknown_fields)]
 struct Guideline {
     angle: Option<String>,
+    filter: Option<String>,
     locked: Option<String>,
     position: String,
 }
@@ -58,18 +62,23 @@ struct Feature {
 #[serde(deny_unknown_fields)]
 struct FontMaster {
     alignment_zones: Option<Vec<String>>,
-    ascender: i32,
-    cap_height: i32,
+    ascender: String,
+    cap_height: String,
     custom_parameters: Option<Vec<CustomParameter>>,
-    descender: i32,
+    custom_value: Option<String>,
+    custom: Option<String>,
+    descender: String,
     guide_lines: Option<Vec<Guideline>>,
     horizontal_stems: Option<Vec<String>>,
+    icon_name: Option<String>,
     id: String,
     user_data: Option<HashMap<String, serde_json::Value>>,
     vertical_stems: Option<Vec<String>>,
     weight_value: Option<String>,
     weight: Option<String>,
-    x_height: i32,
+    width_value: Option<String>,
+    width: Option<String>,
+    x_height: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -80,9 +89,13 @@ struct Instance {
     exports: Option<String>,
     instance_interpolations: HashMap<String, String>,
     interpolation_weight: Option<String>,
+    interpolation_width: Option<String>,
+    is_bold: Option<String>,
+    link_style: Option<String>,
     manual_interpolation: Option<String>,
     name: String,
     weight_class: Option<String>,
+    width_class: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -103,7 +116,54 @@ struct Master {
     right_metrics_key: Option<String>,
     script: Option<String>,
     sub_category: Option<String>,
-    unicode: Option<String>,
+    #[serde(default, deserialize_with = "deserialize_option_unicode")]
+    unicode: Option<Vec<u32>>,
+    // unicode: Option<String>,
+}
+
+fn deserialize_option_unicode<'de, D>(deserializer: D) -> Result<Option<Vec<u32>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    #[derive(Deserialize)]
+    struct Wrapper(#[serde(deserialize_with = "deserialize_unicode")] Vec<u32>);
+
+    let v = Option::deserialize(deserializer)?;
+    Ok(v.map(|Wrapper(a)| a))
+}
+
+fn deserialize_unicode<'de, D>(deserializer: D) -> Result<Vec<u32>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct UnicodeVisitor;
+    impl<'de> Visitor<'de> for UnicodeVisitor {
+        type Value = Vec<u32>;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("a string or int describing a Unicode value as a hex value.")
+        }
+
+        fn visit_u32<E>(self, v: u32) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            self.visit_str(&format!("{:04}", v))
+        }
+
+        fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            let unicodes: Vec<u32> = s
+                .split(",")
+                .map(|e| u32::from_str_radix(e, 16).unwrap())
+                .collect();
+            Ok(unicodes)
+        }
+    }
+
+    deserializer.deserialize_any(UnicodeVisitor)
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -122,7 +182,7 @@ struct Layer {
     name: Option<String>,
     paths: Option<Vec<Path>>,
     user_data: Option<HashMap<String, serde_json::Value>>,
-    width: i32,
+    width: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -133,7 +193,7 @@ struct BackgroundLayer {
     components: Option<Vec<Component>>,
     layer_id: Option<String>,
     paths: Option<Vec<Path>>,
-    width: Option<i32>,
+    width: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -229,11 +289,89 @@ fn main() -> std::io::Result<()> {
     // );
 
     // let f: Font = plist::from_file("data/Empty.glyphs").expect("lol");
-    let f: Font = plist::from_file("data/NewFont.glyphs").expect("lol");
+    // let f: Font = plist::from_file("data/NewFont.glyphs").expect("lol");
+    let f: Font = plist::from_file("data/AnchorAttachmentTest.glyphs").expect("lol");
     println!(
         "{}",
         ron::ser::to_string_pretty(&f, ron::ser::PrettyConfig::default()).unwrap()
     );
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_empty() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/Empty.glyphs")?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_new_font() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/NewFont.glyphs")?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_anchor_attachment() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/AnchorAttachmentTest.glyphs")?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_brace_test_font() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/BraceTestFont.glyphs")?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_bracket_test_font2() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/BracketTestFont2.glyphs")?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_glyphs_unit_test_sans() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/GlyphsUnitTestSans.glyphs")?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_integer_float() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/IntegerFloat.glyphs")?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_cantarell() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/Cantarell.glyphs")?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_worksans() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/WorkSans.glyphs")?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_worksans_italic() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/WorkSans-Italic.glyphs")?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_noto_sans_mm() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/NotoSans-MM.glyphs")?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_ufo_filename_test() -> Result<(), plist::Error> {
+        let _f: Font = plist::from_file("data/UFOFilenameTest.glyphs")?;
+        Ok(())
+    }
 }
